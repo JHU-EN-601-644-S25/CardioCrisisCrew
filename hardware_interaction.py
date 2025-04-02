12cd632e90dec4000516fa741d82894da8a9ef73
Madeline <madeline.f.estey@gmail.com>
2025-04-02 15:28:44 +0100

    button press and sensor

--- hardware_interaction.py
+++ hardware_interaction.py
@@ -1,10 +1,20 @@
-import time
-import time
+import time, sys, signal, atexit
 import gpiod
 from gpiod.line import Direction, Value, Bias
+from __future__ import print_function
+from upm import pyupm_ad8232 as upmAD8232
+
 
 LINE = 27
 B_LINE = 21
+
+myAD8232 = upmAD8232.AD8232(10, 11, 0)
+
+def exitHandler():
+    print("Exiting")
+    sys.exit(0)
+
+atexit.register(exitHandler)
 
 with gpiod.request_lines(
 	"/dev/gpiochip0",
@@ -14,13 +24,25 @@ with gpiod.request_lines(
 		B_LINE: gpiod.LineSettings(direction=Direction.INPUT,bias=Bias.PULL_UP)
     	},
 ) as request:
+	print("Press button to start data collection")
+	collecting = False
+
 	while True:
 		button_state = request.get_value(B_LINE)
 		print(f"Button state: {button_state}")
 		if button_state == Value.INACTIVE:
-			request.set_value(LINE, Value.ACTIVE)
+			if not collecting:
+				print("Starting data collection")
+				time.sleep(2)
+				collecting = True
+				request.set_value(LINE, Value.ACTIVE)
+			print(myAD8232.value())
+			time.sleep(0.001)
 		else:
-			request.set_value(LINE, Value.INACTIVE)
+			if collecting:
+				print("Stopping data collection")
+				collecting = False
+				request.set_value(LINE, Value.INACTIVE)
 		time.sleep(0.1)
         	#request.set_value(LINE, Value.ACTIVE)
         	#time.sleep(1)
