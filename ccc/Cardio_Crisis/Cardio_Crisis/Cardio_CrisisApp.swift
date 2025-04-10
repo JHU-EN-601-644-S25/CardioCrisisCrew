44bffa8f239758d851d9feddbab6cc2d49d7f81a
Lily Wheeler <lwheel16@jh.edu>
2025-04-10 12:34:54 -0400

    add xcode proj

---
+++ ccc/Cardio_Crisis/Cardio_Crisis/Cardio_CrisisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cardio_CrisisApp.swift
+//  Cardio_Crisis
+//
+//  Created by Lily Wheeler on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Cardio_CrisisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ccc/cccTests/cccTests.swift
@@ -0,0 +1,17 @@
+//
+//  cccTests.swift
+//  cccTests
+//
+//  Created by Lily Wheeler on 4/8/25.
+//
+
+import Testing
+@testable import ccc
+
+struct cccTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
