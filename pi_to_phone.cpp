6236a99b93e772e9dc8b375fe3be63499abf1f81
Madeline <madeline.f.estey@gmail.com>
2025-04-10 17:35:56 +0100

    bluetooth progress

--- pi_to_phone.cpp
+++ pi_to_phone.cpp
@@ -1,10 +1,32 @@
-#include "ggk.h"
 #include <iostream>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <signal.h>
+#include <iostream>
+#include <thread>
+#include <sstream>
+#include "external/gobbledegook/include/Gobbledegook.h"
+#include <arpa/inet.h>  //INADDR_ANY
+#include <netinet/in.h>
 
+void LogError(const char *pText) { std::cout << "!!ERROR: " << pText << std::endl; }
+
+static std::string data;
+int server_fd, new_socket;
+struct sockaddr_in address;
+int addrlen = sizeof(address);
+char buffer[1024] = {0};
+
+server_fd = socket(AF_INET, SOCK_STREAM, 0);
+address.sin_family = AF_INET;
+address.sin_addr.s_addr = INADDR_ANY;
+address.sin_port = htons(5000);
+
+bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+listen(server_fd, 3);
+new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
 
 const void* MyDataGetter(const char* pName) {
     	if (pName == nullptr)
@@ -12,23 +34,10 @@ const void* MyDataGetter(const char* pName) {
 		LogError("NULL name sent to server data getter");
 		return nullptr;
 	}
-	static std::string data;
-    	int server_fd, new_socket;
-    	struct sockaddr_in address;
-    	int addrlen = sizeof(address);
-    	char buffer[1024] = {0};
-
-    	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    	address.sin_family = AF_INET;
-    	address.sin_addr.s_addr = INADDR_ANY;
-    	address.sin_port = htons(5000);
-
-    	bind(server_fd, (struct sockaddr*)&address, sizeof(address));
-   	listen(server_fd, 3);
-    	new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
 
 	//read data from Python server
-    	int valread = read(new_socket, buffer, 1024);
+    	std::cout <<"about to read data" << std::endl;
+	int valread = read(new_socket, buffer, 1024);
     	if (valread > 0) {
         	data = std::string(buffer, valread);
 		std::cout << "received data: " << data << std::endl;
@@ -54,6 +63,8 @@ int main() {
 	ggkLogRegisterInfo([](const char* msg) {
         	std::cout << "[GG INFO] " << msg << std::endl;
     	});
+	
+	ggkLogRegisterError(LogError);
 
 	if (!ggkStart("gobbledegook", "Gobbledegook", "Gobbledegook", MyDataGetter, MyDataSetter, 5000)) {
         	std::cerr << "failed to start server" << std::endl;
