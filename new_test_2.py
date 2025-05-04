2164180c99fd35a7ef7dc5bb3d05c3eb9e6f02a7
Madeline <madeline.f.estey@gmail.com>
2025-05-05 00:38:52 +0100

    minor fixes

--- new_test_2.py
+++ new_test_2.py
@@ -35,57 +35,72 @@ def exitHandler():
 
 atexit.register(exitHandler)
 
-with gpiod.request_lines(
-        "/dev/gpiochip0",
+def cleanup(request):
+    try:
+        print("Releasing GPIO lines")
+        request.release() 
+    except Exception as e:
+        print(f"Error releasing GPIO lines: {e}")
+
+time.sleep(2)
+
+try:
+
+	with gpiod.request_lines(
+	"/dev/gpiochip0",
 	consumer="ecg-data-collection",
 	config={
 		LINE: gpiod.LineSettings(direction=Direction.OUTPUT, output_value=Value.INACTIVE),
 		B_LINE: gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP)
 	},
-) as request:
-	print("Press button to start data collection")
-	collecting = False
+	) as request:
+		print("Press button to start data collection")
+		collecting = False
 
-	while True:
-		button_state = request.get_value(B_LINE)
-		#print(f"Button state: {button_state}")
+		while True:
+			button_state = request.get_value(B_LINE)
+			#print(f"Button state: {button_state}")
 
-		if button_state == Value.INACTIVE:
-			if not collecting:
-				print("Starting data collection")
-				time.sleep(1)  # Wait for stabilization
-				collecting = True
-				request.set_value(LINE, Value.ACTIVE)  # Activate the AD8232 sensor
-				time.sleep(2)
+			if button_state == Value.INACTIVE:
+				if not collecting:
+					print("Starting data collection")
+					time.sleep(1)  # Wait for stabilization
+					collecting = True
+					request.set_value(LINE, Value.ACTIVE)  # Activate the AD8232 sensor
+					time.sleep(2)
 
-			#samples.clear()
-			start_time = time.time()
-            		# Read the ECG voltage from the ADS1115
-			while time.time() - start_time < duration:
-        			voltage = chan.voltage
-        			print(f"Collected {chan_name}: {voltage:.3f}V")
-        			samples.append(voltage)
-        			time.sleep(sample_interval)
-			timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-			#filename = f"data_{timestamp}.txt"
-			#with open(filename, "w") as f:
-				#for v in samples:
-					#f.write(f"{v}\n")
-			#print(f"Data written to {filename}")
-			#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			#s.connect(('127.0.0.1', 5000))
+				#samples.clear()
+				start_time = time.time()
+		    		# Read the ECG voltage from the ADS1115
+				while time.time() - start_time < duration:
+					voltage = chan.voltage
+					print(f"Collected {chan_name}: {voltage:.3f}V")
+					samples.append(voltage)
+					time.sleep(sample_interval)
+				timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+				#filename = f"data_{timestamp}.txt"
+				#with open(filename, "w") as f:
+					#for v in samples:
+						#f.write(f"{v}\n")
+				#print(f"Data written to {filename}")
+				#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				#s.connect(('127.0.0.1', 5000))
 
-			msg_lines = [f"{v:.3f}V" for v in samples]
-			msg = "\n".join(msg_lines).encode('utf-8')
-			#s.sendall(msg)
-			save_data_session(samples)
-			print("Data sent successfully.")
-			#s.close()
-		else:
-			if collecting:
-				print("Stopping data collection")
-				collecting = False
-				request.set_value(LINE, Value.INACTIVE)  # Deactivate the AD8232 sensor
-				time.sleep(1)
+				msg_lines = [f"{v:.3f}V" for v in samples]
+				msg = "\n".join(msg_lines).encode('utf-8')
+				#s.sendall(msg)
+				save_data_session(samples)
+				print("Data sent successfully.")
+				#s.close()
+			else:
+				if collecting:
+					print("Stopping data collection")
+					collecting = False
+					request.set_value(LINE, Value.INACTIVE)  # Deactivate the AD8232 sensor
+					time.sleep(1)
 
-		time.sleep(0.1)  # Debounce button press
+			time.sleep(0.1)  # Debounce button press
+except KeyboardInterrupt:
+	print('Interrupted')
+finally:
+	cleanup()
